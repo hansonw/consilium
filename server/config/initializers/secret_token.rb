5d3463d88549dabcfd12cc17acfb6320c18d760f
DouglasSherk <github@sherk.me>
2013-05-27 16:35:37 -0400

    Initial commit of Rails app.

---
+++ server/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Consilium::Application.config.secret_key_base = 'e0d1fc7fd137f55101528988312d626e7064b0c1e873813654968c66ec9b2aa3be1f46d816bd1adf5135cd410382c3a429d3a3e324a45718801f780db9bd01f4'
---
+++ server/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Consilium::Application.config.session_store :cookie_store, key: '_consilium_session'
