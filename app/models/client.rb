2ad62aaadc9d379741bf18b6c1b2a0d87d31ffb4
hanson.wang <hanson.wng@gmail.com>
2013-06-21 18:38:33 -0400

    Basic framework for server-side validation. Also made 'save' a submit action to allow for angular's form validation

--- app/models/client.rb
+++ app/models/client.rb
@@ -28,7 +28,7 @@ class Client
       :name => 'Phone',
       :id => 'phone',
       :placeholder => 'Area code - phone #, ext #',
-      :type => 'phone',
+      :type => 'text',
       :required => true,
     },
     {
@@ -128,4 +128,61 @@ class Client
       ],
     },
   ]
+
+  def validate_scalar(field_name, field_desc, value)
+    if value.nil?
+      if field_desc[:required]
+        errors[field_name] << 'is required'
+      end
+    elsif field_desc[:type] == 'text'
+      value = value.to_s
+      if field_desc[:maxlength] && value.length > field_desc[:maxlength]
+        errors[field_name] << 'is too long'
+      end
+      if field_desc[:minlength] && value.length < field_desc[:minlength]
+        errors[field_name] << 'is too short'
+      end
+    elsif field_desc[:type] == 'int'
+      begin
+        value = Integer(value)
+      rescue
+        errors[field_name] << 'must be an integer'
+        return nil
+      end
+    end
+
+    return value
+  end
+
+  def valid?(context = nil)
+    errors.clear
+    # Custom validation.
+    FIELDS.each do |field|
+      val = self[field[:id]]
+
+      if val.nil?
+        if field[:required]
+          errors[field[:id]] << 'is required'
+        end
+      elsif val['updated_at'].nil?
+        errors[field[:id]] << 'must contain "updated_at"'
+      elsif val['value'].nil?
+        errors[field[:id]] << 'must contain "value"'
+      elsif field[:type].is_a?(Array)
+        if !val['value'].is_a?(Array)
+          errors[field[:id]] << 'must be an array'
+        else
+          val['value'].each_with_index do |subval, i|
+            field[:type].each do |subfield|
+              subval[subfield[:id]] = validate_scalar(subfield[:id], subfield, subval[subfield[:id]])
+            end
+          end
+        end
+      else
+        val['value'] = validate_scalar(field[:id], field, val['value'])
+      end
+    end
+
+    errors.empty? && super(context)
+  end
 end
