3b78b8f567c82b4708b65ac7fe352654a017fee8
DouglasSherk <github@sherk.me>
2013-06-25 10:32:14 -0400

    Add server folder

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   layout 'server'
 
   def index
-    if user_signed_in?
+    if !user_signed_in?
       # AngularJS entry point. Angular should take over from here.
       render nil, :layout => 'angular'
     end
