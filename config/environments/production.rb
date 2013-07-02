facac6592b1db9f6fd2fe6c26b75c5ce03e5f218
DouglasSherk <github@sherk.me>
2013-07-02 17:57:00 -0400

    Fix asset precompilation for server.css, minor styling changes, add production mongoid.yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Consilium::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( server.css, angular.css )
+  config.assets.precompile += %w( server.css angular.css )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
