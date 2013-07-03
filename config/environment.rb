6fe4553d26bf32b7781c26db5d408157238fc5d8
Mark Xu <mark.xu.1994@gmail.com>
2013-07-03 16:11:26 -0400

    Styled the Signup page and the forgot password page

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application.
 Consilium::Application.initialize!
+
+#Stops ruby from overriding the class tags to field_with_errors.
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
