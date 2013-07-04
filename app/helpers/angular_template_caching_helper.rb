86d70c31ef7c879824afb9d6789516c514cadfbf
DouglasSherk <github@sherk.me>
2013-07-04 12:25:49 -0400

    Add simple template caching (doesn't work, need to port this to another technique).

---
+++ app/helpers/angular_template_caching_helper.rb
@@ -0,0 +1,26 @@
+module AngularTemplateCachingHelper
+  def angularAllTemplates
+    templates = {}
+    FileUtils.cd Rails.root.join 'app', 'views', 'templates' do
+      paths = File.join '**', '*.html.erb'
+      Dir.glob(paths) do |path|
+        templates[angularTemplatePath(path)] =
+          angularTemplateContent(path) if angularTemplateIsValid(path)
+      end
+    end
+    puts templates
+    templates
+  end
+
+  def angularTemplateIsValid(f)
+    !File.basename(f).starts_with?('_')
+  end
+
+  def angularTemplatePath(f)
+    "/app/templates/#{File.path(f).sub(/\.erb/, '')}"
+  end
+
+  def angularTemplateContent(f)
+    ERB.new(File.read(f)).src.to_json
+  end
+end
