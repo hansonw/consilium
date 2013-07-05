aec192244a6ec6a2717f66cba47e6d3c70633e67
DouglasSherk <github@sherk.me>
2013-07-04 23:33:25 -0400

    Fix Angular URL targeting.

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -17,7 +17,7 @@ class Api::ApiController < ApplicationController
     # For all responses in this controller, return the CORS access control headers.
     def cors_set_access_control_headers
       headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, PUT'
       headers['Access-Control-Max-Age'] = "1728000"
     end
 
@@ -27,7 +27,7 @@ class Api::ApiController < ApplicationController
     def cors_preflight_check
       if request.method == :options
         headers['Access-Control-Allow-Origin'] = '*'
-        headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+        headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, PUT'
         headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
         headers['Access-Control-Max-Age'] = '1728000'
         render :text => '', :content_type => 'text/plain'
