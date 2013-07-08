e4c2020842bccd619aac769c3b867a9548034569
DouglasSherk <github@sherk.me>
2013-07-08 10:42:58 -0400

    Add phonegap-staging environment.

--- app/helpers/angular_host_helper.rb
+++ app/helpers/angular_host_helper.rb
@@ -1,5 +1,14 @@
 module AngularHostHelper
   def angularApiPath(path)
-    (Rails.env.phonegap? && 'https://consilium.scigit.com' || '')  + path
+    host = ''
+
+    case Rails.env
+    when 'phonegap'
+      host = 'https://consilium.scigit.com'
+    when 'phonegap-staging'
+      host = 'https://consilium-staging.scigit.com'
+    end
+
+    host + path
   end
 end
