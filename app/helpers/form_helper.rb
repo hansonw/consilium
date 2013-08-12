f05bc087934bee60efedab442915ec983fb6f2a7
DouglasSherk <github@sherk.me>
2013-08-12 18:06:00 -0400

    Fix some stupidity.

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -208,25 +208,23 @@ module FormHelper
   def actionHandleDefault(action, id, rowHref)
     newAction = action
 
-    if action == :delete || action == :show || action == :edit
-      if action == :delete
-        newAction = {
-          :icon => 'icon-trash',
-          :class => 'pure-button-error',
-          :click => "deleteFromField('#{id}', null, $index)",
-        }
-      elsif action == :show
-        newAction = {
-          :icon => 'icon-angle-right',
-          :href => rowHref,
-          :class => 'pure-icon',
-        }
-      elsif action == :edit
-        newAction = {
-          :icon => 'icon-edit',
-          :click => "editInField('#{id}', null, $index)"
-        }
-      end
+    if action == :delete
+      newAction = {
+        :icon => 'icon-trash',
+        :class => 'pure-button-error',
+        :click => "deleteFromField('#{id}', null, $index)",
+      }
+    elsif action == :show
+      newAction = {
+        :icon => 'icon-angle-right',
+        :href => rowHref,
+        :class => 'pure-icon',
+      }
+    elsif action == :edit
+      newAction = {
+        :icon => 'icon-edit',
+        :click => "editInField('#{id}', null, $index)"
+      }
     end
 
     newAction
