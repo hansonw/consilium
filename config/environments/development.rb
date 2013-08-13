9e35ace0e4bfd9e2ef58ae2ea51e409419ec122a
hanson.wang <hanson.wng@gmail.com>
2013-08-12 20:20:31 -0400

    Rotate logs at 5mb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,7 @@ Consilium::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # auto rotate log files, keep 2 of 5MB each
+  config.logger = Logger.new(Rails.root.join('log', "#{Rails.env}.log"), 1, 5.megabytes)
 end
