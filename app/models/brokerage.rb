18be4a2bf209bb07729a13dc9fdcbe5e0a57efa6
hanson.wang <hanson.wng@gmail.com>
2013-08-14 18:59:41 -0400

    clients -> num_clients

--- app/models/brokerage.rb
+++ app/models/brokerage.rb
@@ -15,7 +15,7 @@ class Brokerage
   field :website, type: String
   field :phone, type: String
   field :fax, type: String
-  field :clients, type: Integer
+  field :num_clients, type: Integer
   field :employees, type: Integer
 
   field :contacts, type: Array
