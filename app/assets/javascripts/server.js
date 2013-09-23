bb577362b1f62a3d460328afd6b9970248c82149
DouglasSherk <github@sherk.me>
2013-09-23 14:01:50 -0400

    Separate JS into angular and server components. Fixes #53

--- app/assets/javascripts/server.js
+++ app/assets/javascripts/server.js
@@ -10,12 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require util
-//= require modernizr
-//= require jquery
-//= require jquery_ujs
-//= require jquery.ui.datepicker
-//= require jquery.caret
-//= require unstable/angular
-//= require unstable/angular-resource
-//= require_tree .
+//= require shared
+//= require_tree ./server
+//= require_tree ./shared
+//= require_self
