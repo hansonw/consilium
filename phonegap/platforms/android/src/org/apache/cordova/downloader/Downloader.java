2c1d4e2651a328fbacf12eace8eecc6c084f2d3f
hanson.wang <hanson.wng@gmail.com>
2013-10-10 14:25:06 -0400

    Wrap download URLs with auth tokens. Works now!

--- phonegap/platforms/android/src/org/apache/cordova/downloader/Downloader.java
+++ phonegap/platforms/android/src/org/apache/cordova/downloader/Downloader.java
@@ -66,6 +66,8 @@ public class Downloader extends CordovaPlugin {
                               String fileName, CallbackContext callbackContext)
                               throws InterruptedException, JSONException {
     try {
+      showToast("Download started.","short");
+
       File dir = new File(dirName);
       if (!dir.exists()) {
         dir.mkdirs();
@@ -80,25 +82,29 @@ public class Downloader extends CordovaPlugin {
           file = new File(dirName, fileName);
         }
       }
+
       Intent intent = new Intent ();
       intent.addFlags (Intent.FLAG_ACTIVITY_NEW_TASK);
       PendingIntent pend = PendingIntent.getActivity(cordova.getActivity(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
       NotificationManager mNotifyManager = (NotificationManager) cordova.getActivity().getSystemService(Activity.NOTIFICATION_SERVICE);
       NotificationCompat.Builder mBuilder =
-      new NotificationCompat.Builder(cordova.getActivity())
-                                    .setSmallIcon(R.drawable.ic_stat_notification)
-                                    .setContentTitle(cordova.getActivity().getString(R.string.app_name))
-                                    .setContentText("File: " + fileName + " - 0%");
+        new NotificationCompat.Builder(cordova.getActivity())
+                                      .setSmallIcon(R.drawable.ic_stat_notification)
+                                      .setContentTitle(cordova.getActivity().getString(R.string.app_name))
+                                      .setContentText("File: " + fileName + " - 0%");
+
       int mNotificationId = new Random().nextInt(10000);
+      mNotifyManager.notify(mNotificationId, mBuilder.build());
+
       URL url = new URL(fileUrl);
       HttpURLConnection ucon = (HttpURLConnection) url.openConnection();
       ucon.setRequestMethod("GET");
       ucon.connect();
+
       InputStream is = ucon.getInputStream();
       byte[] buffer = new byte[1024];
       int readed = 0, progress = 0, totalReaded = 0, fileSize = ucon.getContentLength();
       FileOutputStream fos = new FileOutputStream(file);
-      showToast("Download started.","short");
       int step = 0;
       while ((readed = is.read(buffer)) > 0) {
         fos.write(buffer, 0, readed);
@@ -118,19 +124,25 @@ public class Downloader extends CordovaPlugin {
       ucon.disconnect();
 
       intent = new Intent(Intent.ACTION_VIEW);
-      intent.setDataAndType(Uri.fromFile(file), URLConnection.guessContentTypeFromName(fileName));
+      String type = URLConnection.guessContentTypeFromName(fileName);
+      if (type == null || type.equals(""))
+        intent.setData(Uri.fromFile(file));
+      else
+        intent.setDataAndType(Uri.fromFile(file), URLConnection.guessContentTypeFromName(fileName));
       PendingIntent openResult = PendingIntent.getActivity(cordova.getActivity(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
       mBuilder.setContentText("Download of \"" + fileName + "\" complete").setProgress(0,0,false);
       mBuilder.setAutoCancel(true);
       mBuilder.setContentIntent(openResult);
       mNotifyManager.notify(mNotificationId, mBuilder.build());
+
       try {
         Thread.sleep(1000);
       } catch (InterruptedException e) {
         Log.d("PhoneGapLog", "Downloader Plugin: Thread sleep error: " + e);
       }
       showToast("Download finished.","short");
+
       if(!file.exists()) {
         mNotifyManager.cancel(mNotificationId);
         showToast("Download went wrong, please try again or contact the developer.","long");
@@ -140,13 +152,13 @@ public class Downloader extends CordovaPlugin {
       return true;
     } catch (FileNotFoundException e) {
       showToast("File does not exists or cannot connect to webserver, please try again or contact the developer.","long");
-      Log.e("PhoneGapLog", "Downloader Plugin: Error: " + PluginResult.Status.ERROR);
+      Log.e("PhoneGapLog", "Downloader Plugin: Error downloading " + fileUrl);
       e.printStackTrace();
       callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR));
       return false;
     } catch (IOException e) {
       showToast("Error downloading file, please try again or contact the developer.","long");
-      Log.e("PhoneGapLog", "Downloader Plugin: Error: " + PluginResult.Status.ERROR);
+      Log.e("PhoneGapLog", "Downloader Plugin: Error downloading " + fileUrl);
       e.printStackTrace();
       callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR));
       return false;
